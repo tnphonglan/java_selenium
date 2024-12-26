b49b8823a34f02a057190f5b278a4df6d87017ea
tnphonglan <tnphonglan@gmail.com>
2024-12-26 17:36:22 +0700

    update_Allure report

---
+++ SeleniumTestNG_Parallel/src/main/java/Automation/reports/AllureReportManager.java
@@ -0,0 +1,20 @@
+package Automation.reports;
+
+import Automation.drivers.DriverManager;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class AllureReportManager {
+    //Text attachments for Allure
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog(String message) {
+        return message;
+    }
+
+    //Screenshot attachments for Allure
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public static byte[] saveScreenshotPNG() {
+        return ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
